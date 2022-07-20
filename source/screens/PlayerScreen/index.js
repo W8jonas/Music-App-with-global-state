5e7657565183436b564364e4b9080d718d3f53ad
Jonas <jonashenrique989@gmail.com>
2022-07-20 02:15:56 -0300

    iniciando tela PlayerScreen

---
+++ source/screens/PlayerScreen/index.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Button } from 'react-native';
+
+export function PlayerScreen({handleUpdateFonte, fontSize}) {
+	return (
+		<View style={{ flex: 1 }}>
+            <View style={{ margin: 20, padding: 20 }}>
+                <Text>PlayerScreen</Text>
+            </View>
+		</View>
+	);
+}
