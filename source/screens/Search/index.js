7aa23ddadc27698afc9a010f4dd64698bc81b60d
Jonas <jonashenrique989@gmail.com>
2022-07-20 02:16:07 -0300

    iniciando tela Search

---
+++ source/screens/Search/index.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Button } from 'react-native';
+
+export function Search({ navigation }) {
+	return (
+		<View style={{ flex: 1 }}>
+            <View style={{ margin: 20, padding: 20 }}>
+                <Text>Search</Text>
+            </View>
+		</View>
+	);
+}
