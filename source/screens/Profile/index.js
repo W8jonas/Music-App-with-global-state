ee26816de035856c521f3b0672e9d5673ad4fd2d
Jonas <jonashenrique989@gmail.com>
2022-07-20 02:16:01 -0300

    iniciando tela Profile

---
+++ source/screens/Profile/index.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Button } from 'react-native';
+
+export function Profile({ navigation }) {
+	return (
+		<View style={{ flex: 1 }}>
+			<View style={{ margin: 20, padding: 20 }}>
+				<Text style={{ margin: 10 }}>Profile</Text>
+			</View>
+		</View>
+	);
+}
