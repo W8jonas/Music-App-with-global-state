845247d5c182fc3e20b800b05d744fb6c08c25a7
Jonas <jonashenrique989@gmail.com>
2022-07-23 21:07:30 -0300

    Add Music

---
+++ source/components/Music/index.js
@@ -0,0 +1,49 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+import defaultImage from '../../assets/emptyMusicValue.jpeg';
+
+export function Music(props) {
+    return (
+
+        <View styles={styles.musicComponent}>
+
+            <Image style={styles.musicImage} source={defaultImage}></Image>
+            <View style={styles.musicText}>
+                <Text style={styles.musicName}>Nome da Música</Text>
+                <Text style={styles.musicArtist}>Artista</Text>
+            </View>
+
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    musicComponent: {
+        margin: 20,
+        // display: 'flex',
+        flexDirection: 'row',
+    },
+
+    musicText: {
+        // display: 'flex',
+        flexDirection: 'column',
+    },
+
+    musicName: {
+        fontSize: 14,
+        color: 'white', 
+    },
+
+    musicArtist: {
+        fontSize: 12,
+        color: 'white', 
+    },
+
+    musicImage: {
+        width: '64.56p',
+        height: '65',
+        borderRadius: '6',
+        backgroundColor: 'gray',
+    }
+
+});
