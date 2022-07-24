9f9761ae5178a53c4a9dda05f5077f85f8b5059d
Jonas <jonashenrique989@gmail.com>
2022-07-23 21:07:49 -0300

    Colocando tela Search

--- source/screens/Search/index.js
+++ source/screens/Search/index.js
@@ -1,12 +1,95 @@
+// import { useState } from 'react';
+// import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Button } from 'react-native';
+
+// export function Search({ navigation }) {
+// 	return (
+// 		<View style={{ flex: 1 }}>
+//             <View style={{ margin: 20, padding: 20 }}>
+//                 <Text>Search</Text>
+//             </View>
+// 		</View>
+// 	);
+// }
+
+
 import { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View, ScrollView, TouchableOpacity, Button } from 'react-native';
+import { Music } from '../../components/Music';
+// import { useFonts, Montserrat_400Regular,} from '@expo-google-fonts/montserrat'
+
 
 export function Search({ navigation }) {
-	return (
-		<View style={{ flex: 1 }}>
-            <View style={{ margin: 20, padding: 20 }}>
-                <Text>Search</Text>
+
+    return (
+        <View style={styles.pageAppearance}>
+
+            <View style={styles.topContainer}>
+
+                <Text style={styles.searchTop}>Pesquisa</Text>
+
+                <StatusBar 
+                  style= "light"
+                  backgroundColor= "blue"
+                  color = "white"
+                />
+
             </View>
-		</View>
-	);
+
+            <View> 
+
+                <Text style={styles.primerText}>Últimas tocadas</Text>
+
+                <Music></Music>
+                <Music></Music>
+                <Music></Music>
+
+            </View>
+
+            <View> 
+
+                <Text style={styles.primerText}>Todas</Text>
+
+                <Music></Music>
+                <Music></Music>
+                <Music></Music>
+
+            </View>
+
+        </View>
+    );
 }
+
+
+const styles = StyleSheet.create({
+
+    pageAppearance: {
+        backgroundColor: 'black',
+        flex: 1,
+    },
+
+    topContainer: {
+        width: '100%',
+        height: 80,
+        backgroundColor: 'blue',
+
+        justifyContent: 'center',
+        marginTop: 20, 
+    },
+
+    searchTop: {
+        fontSize: 24, 
+        color: 'white', 
+        fontWeight: 'bold', 
+        margin: 20, 
+    },
+
+    primerText: {
+        fontSize: 16,
+        color: 'white', 
+        fontWeight: 'bold', 
+        margin: 20,
+    },
+
+});
+
