cf8b892fb3f912cfa60183ae8d249a51b5e9cef4
Jonas <jonashenrique989@gmail.com>
2022-07-24 19:46:40 -0300

    iniciando estrutura PlayerScreen

--- source/screens/PlayerScreen/index.js
+++ source/screens/PlayerScreen/index.js
@@ -1,13 +1,69 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Button } from 'react-native';
-import { Audio } from 'expo-av';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image, Dimensions } from 'react-native';
 
-export function PlayerScreen({handleUpdateFonte, fontSize}) {
+import imgMusic from '../../assets/emptyMusicValue.jpeg'
+
+const screenWidth = Dimensions.get('window').width
+
+const audioBookPlaylist = [
+	{
+		title: 'Hamlet - Act I',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri: 'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act1_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act II',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri: 'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act2_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act III',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri: 'http://www.archive.org/download/hamlet_0911_librivox/hamlet_act3_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act IV',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri: 'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act4_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act V',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri: 'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	}
+]
+
+
+export function PlayerScreen() {
+
 	return (
-		<View style={{ flex: 1 }}>
-            <View style={{ margin: 20, padding: 20 }}>
-                <Text>PlayerScreen</Text>
-            </View>
+		<View style={{ flex: 1, margin: 20, padding: 20, alignItems: 'center', justifyContent: 'space-around' }}>
+
+			<Text>Nome da música</Text>
+
+			<Image style={{ width: screenWidth * 0.9, height: screenWidth * 0.9 }} source={imgMusic} />
+
+			<View>
+				<Text>Nome da música</Text>
+				<Text>Nome da música</Text>
+			</View>
+
+			<View>
+				<Text>Progress bar</Text>
+			</View>
+
+			<View>
+				<Text>Music Menu</Text>
+			</View>
 		</View>
 	);
 }
