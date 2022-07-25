590a26b93197b67d9c634fb1fa2a74df3eafa5b9
Jonas <jonashenrique989@gmail.com>
2022-07-24 21:37:18 -0300

    add função de pause

--- source/screens/PlayerScreen/index.js
+++ source/screens/PlayerScreen/index.js
@@ -6,18 +6,21 @@ import imgMusic from '../../assets/emptyMusicValue.jpeg'
 
 const screenWidth = Dimensions.get('window').width
 
-import {soundsData} from '../../data/sounds'
+import { soundsData } from '../../data/sounds'
 
 export function PlayerScreen() {
 
 	const [sound, setSound] = useState();
 	const [actualSoundData, setActualSoundData] = useState(soundsData[0])
 
-	async function playSound() {
-		const { sound : _sound } = await Audio.Sound.createAsync({uri: actualSoundData.uri});
-		setSound(_sound);
+	const [soundPlayingNow, setSoundPlayingNow] = useState(false)
 
+	async function playSound() {
+		const { sound: _sound } = await Audio.Sound.createAsync({ uri: actualSoundData.uri });
+		setSound(_sound);
+		
 		await _sound.playAsync();
+		setSoundPlayingNow(true)
 	}
 
 	useEffect(() => {
@@ -45,9 +48,14 @@ export function PlayerScreen() {
 
 			const newSoundDataIndex = actualSoundIndex < soundsData.length - 1 ? actualSoundIndex + 1 : soundsData.length - 1
 			setActualSoundData(soundsData[newSoundDataIndex])
-			
+
 			return
 		}
+	}
+
+
+	function pauseSound() {
+		sound.pauseAsync()
 	}
 
 	useEffect(() => {
@@ -62,13 +70,13 @@ export function PlayerScreen() {
 	return (
 		<View style={{ flex: 1, margin: 20, padding: 20, alignItems: 'center', justifyContent: 'space-around' }}>
 
-			<Text>Nome da música</Text>
+			<Text>{actualSoundData.source}</Text>
 
 			<Image style={{ width: screenWidth * 0.9, height: screenWidth * 0.9 }} source={imgMusic} />
 
 			<View>
-				<Text>Nome da música</Text>
-				<Text>Nome da música</Text>
+				<Text>{actualSoundData.title}</Text>
+				<Text>{actualSoundData.author}</Text>
 			</View>
 
 			<View>
@@ -76,15 +84,19 @@ export function PlayerScreen() {
 			</View>
 
 			<View>
-				<TouchableOpacity onPress={() => {handleChangeActualSound('-')}}>
+				<TouchableOpacity onPress={() => { handleChangeActualSound('-') }}>
 					<Text>Anterior</Text>
 				</TouchableOpacity>
 
-				<TouchableOpacity onPress={playSound}>
-					<Text>Iniciar</Text>
-				</TouchableOpacity>
+				{soundPlayingNow ? <TouchableOpacity onPress={pauseSound}>
+						<Text>Pausar</Text>
+					</TouchableOpacity> 
+					: <TouchableOpacity onPress={playSound}>
+						<Text>Iniciar</Text>
+					</TouchableOpacity>
+				}
 
-				<TouchableOpacity onPress={() => {handleChangeActualSound('+')}}>
+				<TouchableOpacity onPress={() => { handleChangeActualSound('+') }}>
 					<Text>Próxima</Text>
 				</TouchableOpacity>
 
