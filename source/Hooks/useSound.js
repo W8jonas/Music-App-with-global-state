e448a1eda2dd701fdc45c8dd2be0724850099a77
Jonas <jonashenrique989@gmail.com>
2022-08-14 18:51:33 -0300

    Criando hook useSound

---
+++ source/Hooks/useSound.js
@@ -0,0 +1,64 @@
+import { Audio } from 'expo-av';
+import { useEffect, useState } from 'react';
+
+
+export function useSound(soundsData) {
+
+    const [sound, setSound] = useState();
+	const [actualSoundData, setActualSoundData] = useState(soundsData[0])
+
+	const [soundPlayingNow, setSoundPlayingNow] = useState(false)
+
+
+	async function playSound(_newSoundToPlay) {
+
+		const newSoundToPlay = _newSoundToPlay ? _newSoundToPlay : actualSoundData
+
+		const { sound: _sound, status } = await Audio.Sound.createAsync({ uri: newSoundToPlay.uri });
+
+		setSound(_sound);
+		setActualSoundData({...newSoundToPlay, details: status})
+
+		await _sound.playAsync();
+		setSoundPlayingNow(true)
+	}
+
+
+	function handleChangeActualSound(changeType) {
+
+		const actualSoundIndex = soundsData.findIndex(sounds => sounds.id === actualSoundData.id ? true : false)
+
+		if (changeType === '-') {
+			const newSoundDataIndex = actualSoundIndex > 0 ? actualSoundIndex - 1 : 0
+			playSound(soundsData[newSoundDataIndex])
+
+			return
+		}
+
+		if (changeType === '+') {
+			const newSoundDataIndex = actualSoundIndex < soundsData.length - 1 ? actualSoundIndex + 1 : soundsData.length - 1
+			playSound(soundsData[newSoundDataIndex])
+
+			return
+		}
+	}
+
+
+	function pauseSound() {
+		sound.pauseAsync()
+		setSoundPlayingNow(false)
+	}
+
+
+	useEffect(() => {
+		return sound
+			? () => {
+				sound.unloadAsync();
+			}
+			: undefined;
+	}, [sound]);
+
+    return {
+        sound, actualSoundData, soundPlayingNow, soundsData, playSound, handleChangeActualSound, pauseSound
+    }
+}
