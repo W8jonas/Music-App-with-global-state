6eb5419b3e995de466348284e9c2b8470c49daf4
Jonas <jonashenrique989@gmail.com>
2022-08-14 18:51:41 -0300

    refatorando para useSound

--- source/routes/index.js
+++ source/routes/index.js
@@ -6,9 +6,8 @@ import { Home } from '../screens/Home';
 import { PlayerScreen } from '../screens/PlayerScreen';
 import { Search } from '../screens/Search';
 import { Profile } from '../screens/Profile';
-import { Audio } from 'expo-av';
-import { useEffect, useState } from 'react';
 import { soundsData } from '../data/sounds';
+import { useSound } from '../Hooks/useSound';
 
 
 
@@ -38,64 +37,7 @@ const Tabs = createBottomTabNavigator();
 
 export function Routes() {
 
-    const [sound, setSound] = useState();
-	const [actualSoundData, setActualSoundData] = useState(soundsData[0])
-
-	const [soundPlayingNow, setSoundPlayingNow] = useState(false)
-
-
-	async function playSound(_newSoundToPlay) {
-
-		const newSoundToPlay = _newSoundToPlay ? _newSoundToPlay : actualSoundData
-
-		const { sound: _sound, status } = await Audio.Sound.createAsync({ uri: newSoundToPlay.uri });
-
-		setSound(_sound);
-		setActualSoundData({...newSoundToPlay, details: status})
-
-		await _sound.playAsync();
-		setSoundPlayingNow(true)
-	}
-
-
-	function handleChangeActualSound(changeType) {
-
-		const actualSoundIndex = soundsData.findIndex(sounds => sounds.id === actualSoundData.id ? true : false)
-
-		if (changeType === '-') {
-			const newSoundDataIndex = actualSoundIndex > 0 ? actualSoundIndex - 1 : 0
-			playSound(soundsData[newSoundDataIndex])
-
-			return
-		}
-
-		if (changeType === '+') {
-			const newSoundDataIndex = actualSoundIndex < soundsData.length - 1 ? actualSoundIndex + 1 : soundsData.length - 1
-			playSound(soundsData[newSoundDataIndex])
-
-			return
-		}
-	}
-
-
-	function pauseSound() {
-		sound.pauseAsync()
-		setSoundPlayingNow(false)
-	}
-
-
-	useEffect(() => {
-		return sound
-			? () => {
-				sound.unloadAsync();
-			}
-			: undefined;
-	}, [sound]);
-
-
-    const globalSound = {
-        sound, actualSoundData, soundPlayingNow, soundsData, playSound, handleChangeActualSound, pauseSound
-    }
+    const globalSound = useSound(soundsData)
 
 	return (
 		<NavigationContainer>
