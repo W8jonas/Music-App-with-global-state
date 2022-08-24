b94d33ad3150c09f2b6fa3e24f18992edc27e84d
Jonas <jonashenrique989@gmail.com>
2022-08-23 23:50:39 -0300

    Removendo comments

--- source/Hooks/useSound.js
+++ source/Hooks/useSound.js
@@ -9,22 +9,7 @@ export function useSound(soundsData) {
 
 	const [soundPlayingNow, setSoundPlayingNow] = useState(false)
 
-
-	// async function playSound(_newSoundToPlay) {
-	// 	console.log('playSound', _newSoundToPlay)
-	// 	const newSoundToPlay = _newSoundToPlay ? _newSoundToPlay : actualSoundData
-
-	// 	const { sound: _sound, status } = await Audio.Sound.createAsync({ uri: newSoundToPlay.uri });
-
-	// 	setSound(_sound);
-	// 	setActualSoundData({...newSoundToPlay, details: status})
-
-	// 	await _sound.playAsync();
-	// 	setSoundPlayingNow(true)
-	// }
-
 	const playSound = useCallback(async (_newSoundToPlay) => {
-		console.log('playSound', _newSoundToPlay)
 		const newSoundToPlay = _newSoundToPlay ? _newSoundToPlay : actualSoundData
 
 		const { sound: _sound, status } = await Audio.Sound.createAsync({ uri: newSoundToPlay.uri });
@@ -35,25 +20,6 @@ export function useSound(soundsData) {
 		await _sound.playAsync();
 		setSoundPlayingNow(true)
 	}, [actualSoundData])
-
-
-	// function handleChangeActualSound(changeType) {
-	// 	const actualSoundIndex = soundsData.findIndex(sounds => sounds.id === actualSoundData.id ? true : false)
-
-	// 	if (changeType === '-') {
-	// 		const newSoundDataIndex = actualSoundIndex > 0 ? actualSoundIndex - 1 : 0
-	// 		playSound(soundsData[newSoundDataIndex])
-
-	// 		return
-	// 	}
-
-	// 	if (changeType === '+') {
-	// 		const newSoundDataIndex = actualSoundIndex < soundsData.length - 1 ? actualSoundIndex + 1 : soundsData.length - 1
-	// 		playSound(soundsData[newSoundDataIndex])
-
-	// 		return
-	// 	}
-	// }
 
 	const handleChangeActualSound = useCallback((changeType) => {
 		const actualSoundIndex = soundsData.findIndex(sounds => sounds.id === actualSoundData.id ? true : false)
@@ -72,11 +38,6 @@ export function useSound(soundsData) {
 			return
 		}
 	}, [soundsData, actualSoundData])
-
-	// function pauseSound() {
-	// 	sound.pauseAsync()
-	// 	setSoundPlayingNow(false)
-	// }
 
 	const pauseSound = useCallback(() => {
 		sound.pauseAsync()
